441befd4be35981b3909039453fe49dae7be6024
jackonedev <jackone.action.software@gmail.com>
2023-12-04 08:40:03 -0300

    concluded airflow hook with basic connection handling and with extra functionalities

--- custom_operators/hooks/movielens.py
+++ custom_operators/hooks/movielens.py
@@ -30,3 +30,46 @@ class MovieLensHook(BaseHook):
             self._base_url = base_url
         
         return self._session, self._base_url
+    
+    def get_ratings(self, start_date=None, end_date=None, batch_size=100):
+        """
+        Fetches rating between the given start/end date.
+        Parameters
+        ————— 
+        start_date : str
+            Start date to start fetching ratings from (inclusive). Expected
+            format is YYYY-MM-DD (equal to Airflow"s ds formats).
+        end_date : str
+            End date to fetching ratings up to (exclusive). Expected
+            format is YYYY-MM-DD (equal to Airflow"s ds formats).
+        batch_size : int
+            Size of the batches (pages) to fetch from the API. Larger values
+            mean less requests, but more data transferred per request.
+        """
+        yield from self._get_with_pagination(
+            endpoint="/ratings",
+            params={"start_date": start_date, "end_date": end_date},
+            batch_size=batch_size,
+        )
+        
+    def _get_with_pagination(self, endpoint, params, batch_size=100):
+        """
+        Fetches records using a get request with given url/params,
+        taking pagination into account.
+        """
+        session, base_url = self.get_conn()
+        offset = 0
+        total = None
+        while total is None or offset < total:
+            response = session.get(base_url, params={
+                    **params,
+                    **{"offset": offset, "limit": batch_size}
+                }
+            )
+            response.raise_for_status()
+            response_json = response.json()
+            
+            yield from response_json["result"]
+            
+            offset += batch_size
+            total = response_json["total"]
