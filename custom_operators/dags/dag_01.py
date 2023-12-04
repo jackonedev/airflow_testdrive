19cb7259d88bf13819d4f97a5b780762f65ad482
jackonedev <jackone.action.software@gmail.com>
2023-12-04 07:30:53 -0300

    created DAG for 'fetch_ratings' and 'rank_movies' operators

---
+++ custom_operators/dags/dag_01.py
@@ -0,0 +1,18 @@
+from airflow.models import DAG
+from airflow.utils.dates import datetime
+import pandas as pd
+
+
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+from custom_operators.operators.fetch_ratings import fetch_ratings
+from custom_operators.operators.rank_movies import rank_movies
+
+with DAG(
+    dag_id="custom_operators",
+    start_date=datetime(2021, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+    fetch_ratings >> rank_movies
