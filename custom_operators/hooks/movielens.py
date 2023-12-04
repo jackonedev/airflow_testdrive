22df09cb53128e819a31dd3b5423a03afd639aa5
jackonedev <jackone.action.software@gmail.com>
2023-12-04 08:31:47 -0300

    using internal method for connecting with airflow metastore and preserving connection

--- custom_operators/hooks/movielens.py
+++ custom_operators/hooks/movielens.py
@@ -11,12 +11,22 @@ class MovieLensHook(BaseHook):
     def __init__(self, conn_id):
         super().__init__()
         self._conn_id = conn_id
-
+        self.DEFAULT_SCHEMA = MOVIELENS_SCHEMA
+        self.DEFAULT_HOST = MOVIELENS_HOST
+        self.DEFAULT_PORT = MOVIELENS_PORT
+        self._session = None
+        self._base_url = None
+        
     def get_conn(self):
-        session = requests.Session()
-        session.auth = (MOVIELENS_USER, MOVIELENS_PASSWORD)
-        schema = MOVIELENS_SCHEMA
-        host = MOVIELENS_HOST
-        port = MOVIELENS_PORT
-        base_url = f"{schema}://{host}:{port}"
-        return session, base_url
+        if self._session is None:
+            config = self.get_connection(self._conn_id)
+            session = requests.Session()
+            session.auth = (MOVIELENS_USER, MOVIELENS_PASSWORD)
+            schema = config.schema or self.DEFAULT_SCHEMA
+            host = config.host or self.DEFAULT_HOST
+            port = config.port or self.DEFAULT_PORT
+            base_url = f"{schema}://{host}:{port}"
+            self._session = session
+            self._base_url = base_url
+        
+        return self._session, self._base_url
